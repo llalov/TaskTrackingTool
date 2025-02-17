0625fd75ff0eb6ccedd90f42524b10b8d44e77b4
Lachezar Lalov <luchoazz@abv.bg>
2025-02-17 21:57:37 +0200

    added components file structure and set-up react router

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div>
+        <h1>Task Tracking</h1>
+        {/* <Header/> */}
+        <main>
+          
+        </main>
+    </div>
+  )
+}
+
+export default App
