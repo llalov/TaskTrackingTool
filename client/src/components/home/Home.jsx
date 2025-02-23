3aa004cf44d63d4d06edd6499b85a4f024bae87f
Lachezar Lalov <lachezar.lalov.91@gmail.com>
2025-02-24 00:48:54 +0200

    updated home page links

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export default function Home() {
     return(
         <div className="bg-white">
@@ -24,15 +26,15 @@ export default function Home() {
               Ac euismod vel sit maecenas id pellentesque eu sed consectetur. Malesuada adipiscing sagittis vel nulla.
             </p> */}
             <div className="mt-10 flex items-center justify-center gap-x-6 lg:justify-start">
-              <a
-                href="/login"
+              <Link
+                to="/login"
                 className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-xs hover:bg-gray-100 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
               >
                 Sign in or Register
-              </a>
-              <a href="#" className="text-sm/6 font-semibold text-white">
+              </Link>
+              <Link to="/about" className="text-sm/6 font-semibold text-white">
                 Learn more <span aria-hidden="true">→</span>
-              </a>
+              </Link>
             </div>
           </div>
           <div className="relative mt-16 h-80 lg:mt-8">
